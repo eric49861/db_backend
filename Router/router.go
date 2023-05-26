df0a25b114c162044b51ed7f629e3c4763417152
eric <15854168262@qq.com>
2023-05-26 17:49:25 +0800

    添加后台路由接口

--- Router/router.go
+++ Router/router.go
@@ -1,1 +1,30 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	middle "studygroup-gin/Middleware"
+	service "studygroup-gin/Service"
+)
+
+func RegisteRouter(r *gin.Engine) {
+	v1 := r.Group("/v1")
+	{
+		free := v1.Group("/1")
+		{
+			free.POST("/login", service.DoLogin())
+			free.POST("/signup", service.DoSignup())
+		}
+		//该类接口需要token验证
+		authentication := v1.Group("/2")
+		{
+			authentication.Use(middle.JWTAuthMiddleware())
+			authentication.GET("/get-all-group", service.GetGroupList())
+		}
+
+		// 其他的一些工具接口
+		tool := v1.Group("/3")
+		{
+			tool.GET("/valid-token", service.ValidToken())
+		}
+	}
+}
