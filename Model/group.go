e8f87904ada10b116c9108672ca20c4b5825198b
eric <15854168262@qq.com>
2023-05-26 17:46:00 +0800

    定义小组模型

--- Model/group.go
+++ Model/group.go
@@ -1,13 +1,17 @@
 package model
 
 type Group struct {
-	Id         uint   `gorm:"primaryKey;autoIncrement;column:id"`
-	CreatorId  uint   `gorm:"not null;column:creator_id"`
-	GroupName  string `gorm:"not null;column:group_name"`
-	Topic      string `gorm:"not null;column:topic"`
-	Notice     string `gorm:"not null;column:notice"`
-	CreateTime int64  `gorm:"not null;autoCreateTime:nano;column:create_time"`
-	EndTime    int64  `gorm:"not null;column:end_time"`
-	State      bool   `gorm:"not null;column:state"`
-	Remarks    string `gorm:"not null;column:notice"`
+	Id         uint   `json:"id" gorm:"primaryKey;autoIncrement;column:id"`
+	CreatorId  uint   `json:"creatorId" gorm:"not null;column:creator_id"`
+	GroupName  string `json:"groupName" gorm:"not null;column:group_name"`
+	Topic      string `json:"topic" gorm:"not null;column:topic"`
+	Notice     string `json:"notice" gorm:"not null;column:notice"`
+	CreateTime int64  `json:"createTime" gorm:"not null;autoCreateTime:nano;column:create_time"`
+	EndTime    int64  `json:"endTime" gorm:"not null;column:end_time"`
+	State      bool   `json:"state" gorm:"not null;column:state"`
+	Remarks    string `json:"remarks" gorm:"not null;column:remarks"`
+}
+
+func (Group) TableName() string {
+	return "group"
 }
