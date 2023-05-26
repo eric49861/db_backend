b9591e73002dc54c04b157a2652635ae5afe1ce7
eric <15854168262@qq.com>
2023-05-26 22:32:18 +0800

    问题表模型，添加json映射

--- Model/question.go
+++ Model/question.go
@@ -1,10 +1,14 @@
 package model
 
 type Question struct {
-	Id         uint   `gorm:"primaryKey;column:id"`
-	CreatorId  uint   `gorm:"not null;column:creator_id"`
-	GroupId    uint   `gorm:"not null;column:group_id"`
-	Likes      uint   `gorm:"not null;column:likes"`
-	Content    string `gorm:"not null;column:content"`
-	CreateTime int64  `gorm:"autoCreateTime:nano;column:create_time"`
+	Id         uint   `json:"id" gorm:"primaryKey;column:id"`
+	CreatorId  uint   `json:"creator_id" gorm:"not null;column:creator_id"`
+	GroupId    uint   `json:"group_id" gorm:"not null;column:group_id"`
+	Likes      uint   `json:"likes" gorm:"not null;column:likes"`
+	Content    string `json:"content" gorm:"not null;column:content"`
+	CreateTime int64  `json:"create_time" gorm:"autoCreateTime:nano;column:create_time"`
+}
+
+func (Question) TableName() string {
+	return "question"
 }
