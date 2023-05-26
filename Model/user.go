9f9a672a3b696827ff818d27fcf364fe24b5d638
eric <15854168262@qq.com>
2023-05-26 18:02:41 +0800

    修改user模型的定义，添加性别字段

--- Model/user.go
+++ Model/user.go
@@ -4,6 +4,7 @@ type User struct {
 	Id         uint   `json:"id" gorm:"primaryKey"`
 	Name       string `json:"username" gorm:"not null;column:username"`
 	Password   string `json:"password" gorm:"not null;column:password"`
+	Gender     string `json:"gender" gorm:"not null;column:gender"`
 	QQ         string `json:"qq" gorm:"not null;column:qq"`
 	Integral   uint   `json:"integral" gorm:"not null;column:integral"`
 	CreateTime int64  `json:"createTime" gorm:"autoCreateTime:nano;column:create_time"`
