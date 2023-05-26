3bcde98a6ff82001c0e9eb8efe48305813c9ae45
eric <15854168262@qq.com>
2023-05-26 17:45:35 +0800

    定义用户模型

--- Model/user.go
+++ Model/user.go
@@ -1,11 +1,15 @@
 package model
 
 type User struct {
-	Id         uint `gorm:"primaryKey"`
-	Name       string
-	Password   string
-	QQ         string
-	Integral   uint
-	CreateTime int64
-	UpdateTime int64
+	Id         uint   `json:"id" gorm:"primaryKey"`
+	Name       string `json:"username" gorm:"not null;column:username"`
+	Password   string `json:"password" gorm:"not null;column:password"`
+	QQ         string `json:"qq" gorm:"not null;column:qq"`
+	Integral   uint   `json:"integral" gorm:"not null;column:integral"`
+	CreateTime int64  `json:"createTime" gorm:"autoCreateTime:nano;column:create_time"`
+	UpdateTime int64  `json:"updateTime" gorm:"autoCreateTime:nano;column:create_time"`
+}
+
+func (User) TableName() string {
+	return "user"
 }
