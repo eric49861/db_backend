20c2cc42545860f2253c43a16ce0310246fd8dd1
eric <15854168262@qq.com>
2023-05-26 17:44:53 +0800

    定义加密函数

--- util/encrypt.go
+++ util/encrypt.go
@@ -1,1 +1,13 @@
 package util
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// EncryptWithMD5 采用MD5算法对字符串进行加密，对网站用户的密码加密就是调用该函数
+func EncryptWithMD5(targetStr string) string {
+	md5Byte := md5.Sum([]byte(targetStr))
+
+	return hex.EncodeToString(md5Byte[:])
+}
