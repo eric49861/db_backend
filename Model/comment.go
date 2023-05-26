3aa9c63d630542a2ccb99359551d11211281031f
eric <15854168262@qq.com>
2023-05-26 22:30:58 +0800

    评论表模型

--- Model/comment.go
+++ Model/comment.go
@@ -1,11 +1,15 @@
 package model
 
 type Comment struct {
-	Id         uint   `gorm:"primaryKey;column:id"`
-	CreatorId  uint   `gorm:"not null;column:creator_id"`
-	QuestionId uint   `gorm:"not null;column:question_id"`
-	CommentId  uint   `gorm:"not null;column:comment_id"`
-	Content    string `gorm:"not null;column:content"`
-	Likes      uint   `gorm:"not null;column:likes"`
-	CreateTime int64  `gorm:"autoCreateTime:nano;column:create_time"`
+	Id         uint   `json:"id" gorm:"primaryKey;column:id"`
+	CreatorId  uint   `json:"creator_id" gorm:"not null;column:creator_id"`
+	QuestionId uint   `json:"question_id" gorm:"not null;column:question_id"`
+	CommentId  uint   `json:"comment_id" gorm:"not null;column:comment_id"`
+	Content    string `json:"content" gorm:"not null;column:content"`
+	Likes      uint   `json:"likes" gorm:"not null;column:likes"`
+	CreateTime int64  `json:"create_time" gorm:"autoCreateTime:nano;column:create_time"`
+}
+
+func (Comment) TableName() string {
+	return "comment"
 }
