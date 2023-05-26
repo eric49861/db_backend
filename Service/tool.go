18bc5de7da27fc3ecdb94e1ff2682110eaca5e2b
eric <15854168262@qq.com>
2023-05-26 17:48:10 +0800

    添加工具类服务接口

--- Service/tool.go
+++ Service/tool.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"studygroup-gin/util"
+)
+
+func ValidToken() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		tokenString := context.Query("token")
+		fmt.Println("tokenStirng: ", tokenString)
+		if util.IsValidTokenString(tokenString) {
+			context.JSON(http.StatusOK, gin.H{
+				"msg": "success",
+			})
+		} else {
+			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"msg": "非法token",
+			})
+		}
+	}
+}
