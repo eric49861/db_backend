0d8ee4631512735980a4d742ed9264cf84145173
eric <15854168262@qq.com>
2023-05-26 22:33:03 +0800

    资源表模型，添加json映射

--- Model/resource.go
+++ Model/resource.go
@@ -1,10 +1,14 @@
 package model
 
 type Resource struct {
-	Id         uint   `gorm:"primaryKey;column:id"`
-	UserId     uint   `gorm:"column:user_id"`
-	GroupId    uint   `gorm:"not null;column:group_id"`
-	Content    string `gorm:"not null;column:content"`
-	UploadTime int64  `gorm:"not null;column:upload_time"`
-	UpdateTime int64  `gorm:"autoCreateTime:nano;column:update_time"`
+	Id         uint   `json:"id" gorm:"primaryKey;column:id"`
+	UserId     uint   `json:"user_id" gorm:"column:user_id"`
+	GroupId    uint   `json:"group_id" gorm:"not null;column:group_id"`
+	Content    string `json:"content" gorm:"not null;column:content"`
+	UploadTime int64  `json:"upload_time" gorm:"not null;column:upload_time"`
+	UpdateTime int64  `json:"update_time" gorm:"autoCreateTime:nano;column:update_time"`
+}
+
+func (Resource) TableName() string {
+	return "resource"
 }
