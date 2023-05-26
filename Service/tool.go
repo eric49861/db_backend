4fa7764b891dc47bbd2445229f22931f406d5a41
eric <15854168262@qq.com>
2023-05-26 22:33:55 +0800

    添加服务工具类

--- Service/tool.go
+++ Service/tool.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"studygroup-gin/util"
+	util "studygroup-gin/util"
 )
 
 func ValidToken() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		tokenString := context.Query("token")
+		tokenString := context.PostForm("token")
 		fmt.Println("tokenStirng: ", tokenString)
 		if util.IsValidTokenString(tokenString) {
 			context.JSON(http.StatusOK, gin.H{
-				"msg": "success",
+				"code": http.StatusOK,
+				"msg":  "success",
 			})
 		} else {
 			context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"msg": "非法token",
+				"code": http.StatusUnauthorized,
+				"msg":  "非法token",
 			})
 		}
 	}
