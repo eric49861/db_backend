937ed0cf7c6c0334ee3e67d1eedf1dae8ee8a534
eric <15854168262@qq.com>
2023-05-26 17:44:10 +0800

    定义跨域中间件

---
+++ Middleware/cros.go
@@ -0,0 +1,22 @@
+package middle
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Cors 解决前后端跨域
+func Cors() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		method := context.Request.Method
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, Token, New-Token")
+		context.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+		}
+		context.Next()
+	}
+}
