9b3045f2ea856b6ae4cd8b387c2bdec6834bf821
eric <15854168262@qq.com>
2023-05-28 12:20:27 +0800

    修改用户表模型，添加更多描述用户的字段

--- Model/user.go
+++ Model/user.go
@@ -1,14 +1,18 @@
 package model
 
 type User struct {
-	Id         uint   `json:"id" gorm:"primaryKey"`
-	Name       string `json:"username" gorm:"not null;column:username"`
-	Password   string `json:"password" gorm:"not null;column:password"`
-	Gender     string `json:"gender" gorm:"not null;column:gender"`
-	QQ         string `json:"qq" gorm:"not null;column:qq"`
-	Integral   uint   `json:"integral" gorm:"not null;column:integral;default:0"`
-	CreateTime int64  `json:"createTime" gorm:"autoCreateTime:nano;column:create_time"`
-	UpdateTime int64  `json:"updateTime" gorm:"autoCreateTime:nano;column:create_time"`
+	Id         uint    `json:"id" gorm:"primaryKey"`
+	Name       string  `json:"username" gorm:"not null;column:username"`
+	Password   string  `json:"password" gorm:"not null;column:password"`
+	Signature  string  `json:"signature" gorm:"not null;column:signature"`
+	Avatar     string  `json:"avatar" gorm:"not null;column:avatar"`
+	Gender     string  `json:"gender" gorm:"not null;column:gender"`
+	QQ         string  `json:"qq" gorm:"not null;column:qq"`
+	Integral   uint    `json:"integral" gorm:"not null;column:integral;default:0"`
+	Occupation string  `json:"occupation" gorm:"not null;column:occupation"`
+	StudyTime  float64 `json:"studyTime" gorm:"not null;column:study_time"`
+	CreateTime int64   `json:"createTime" gorm:"autoCreateTime:nano;column:create_time"`
+	UpdateTime int64   `json:"updateTime" gorm:"autoCreateTime:nano;column:create_time;default:"`
 }
 
 func (User) TableName() string {
